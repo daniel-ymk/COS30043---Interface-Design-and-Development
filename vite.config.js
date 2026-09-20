892ef649f93aa79f550321a0460c63833386b4bb
daniel-ymk <yemyatkyaw.daniel@gmail.com>
2026-09-20 18:30:57 +0800

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/COS30043---Interface-Design-and-Development',
+  base: '/COS30043---Interface-Design-and-Development/',
 })
